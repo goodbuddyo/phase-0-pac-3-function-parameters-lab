96fa954ee35b089b3305463f934a4c6eb7a97157
Seano <sean01234@yahoo.com>
2023-03-05 00:21:43 -0500

    address spacing to match test

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
 function introduction(name) {
-  console.log(`Hi, my name is ${name}`);
+  console.log(`Hi, my name is ${name}.`);
+  return `Hi, my name is ${name}.`;
 }
-introduction("Superman");
+// introduction("Aki");
 
 function logTwoValues(value_1, value_2) {
   console.log(`The two values are ${value_1} and ${value_2}`);
 }
-logTwoValues("a", "z");
+// logTwoValues("a", "z");
 
 // create two params
 function introductionWithLanguage(name, language) {
-  console.log(`Hi, my name is  ${name} and I am learning to program in ${language}.`);
+  console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
-introductionWithLanguage("Sven", "Go");
+// introductionWithLanguage("Samip", "React");
 
 //set default argument value
 function sayHelloTo(firstName = "User") {
   console.log(`Hello, ${firstName}`);
 }
-sayHelloTo();
+// sayHelloTo();
 
+// set default value for second parameter
 function introductionWithLanguageOptional(name, language = "JavaScript") {
   console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
-introductionWithLanguageOptional("Buster");
+// introductionWithLanguageOptional("Gracie");
 
 
