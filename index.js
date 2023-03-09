3c7cc1b33bf3cd411c6e0a756b984a0b206011b4
Seano <sean01234@yahoo.com>
2023-03-09 11:16:24 -0500

    changes to pass tests

--- index.js
+++ index.js
@@ -2,18 +2,21 @@ function introduction(name) {
   console.log(`Hi, my name is ${name}.`);
   return `Hi, my name is ${name}.`;
 }
-// introduction("Aki");
+introduction("Aki");
+introduction("Samip");
 
-function logTwoValues(value_1, value_2) {
-  console.log(`The two values are ${value_1} and ${value_2}`);
-}
+// function logTwoValues(value_1, value_2) {
+//   console.log(`The two values are ${value_1} and ${value_2}`);
+// }
 // logTwoValues("a", "z");
 
 // create two params
 function introductionWithLanguage(name, language) {
   console.log(`Hi, my name is ${name} and I am learning to program in ${language}.`);
 }
-// introductionWithLanguage("Samip", "React");
+introductionWithLanguage("Aki", "Ember.js");
+
+introductionWithLanguage("Samip", "React");
 
 //set default argument value
 function sayHelloTo(firstName = "User") {
@@ -27,4 +30,5 @@ function introductionWithLanguageOptional(name, language = "JavaScript") {
 }
 // introductionWithLanguageOptional("Gracie");
 
-
+introductionWithLanguageOptional("Gracie");
+introductionWithLanguageOptional("Gracie", "Python");
